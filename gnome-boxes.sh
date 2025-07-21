a96e68a2693e8c1982da69ecdecfaa1c89859596
NomaCasa <nomapi@pm.me>
2025-07-21 10:49:40 -0400

    Update gnome-boxes.sh

--- gnome-boxes.sh
+++ gnome-boxes.sh
@@ -1,14 +1,17 @@
-[properties]
-name=Tumbleweed-Desktop
-ram=16384
-cpu=4
-disk-size=204800
-os-type=linux
-os-variant=opensuse-tumbleweed
+#!/bin/bash
+source .secrets
 
-[drivers]
-nvidia=yes
-vga=virtio
+# Create virtual disk
+qemu-img create -f qcow2 tumbleweed.qcow2 200G
 
-[network]
-bridge=virbr0
+# Install with virt-install
+virt-install \
+  --name Tumbleweed-Desktop \
+  --memory 16384 \
+  --vcpus 4 \
+  --disk tumbleweed.qcow2 \
+  --cdrom openSUSE-Tumbleweed-NET-x86_64-Current.iso \
+  --network bridge=virbr0 \
+  --graphics spice \
+  --os-variant opensuse-tumbleweed \
+  --boot uefi
