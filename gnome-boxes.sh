47bd56d392f1856ade7d9f38894f55051c5f6ab6
NomaCasa <nomapi@pm.me>
2025-07-21 10:49:14 -0400

    Create tumbleweed.metadata

---
+++ gnome-boxes.sh
@@ -0,0 +1,14 @@
+[properties]
+name=Tumbleweed-Desktop
+ram=16384
+cpu=4
+disk-size=204800
+os-type=linux
+os-variant=opensuse-tumbleweed
+
+[drivers]
+nvidia=yes
+vga=virtio
+
+[network]
+bridge=virbr0
